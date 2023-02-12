70d613a9f6ada45682844d284cf15028557cf734
Mari-An-test <120311973+Mari-An-test@users.noreply.github.com>
2023-02-12 16:40:53 +0300

    Поправила

--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -4,9 +4,12 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.mockito.Mock;
 
 @RunWith(Parameterized.class)
 public class LionTest {
+    @Mock
+    Feline feline;
     private final String sex;
     private final boolean expectedHasMane;
 
@@ -27,5 +30,17 @@ public class LionTest {
         Lion lion = new Lion(sex, new Feline());
         boolean actualHasMane = lion.doesHaveMane();
         Assert.assertEquals(expectedHasMane, actualHasMane);
+    }
+
+    @Test
+    public void doesHaveManeMaleReturnsTrue() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+        Assert.assertTrue("Самец doesHaveMane() должен вернуть true", lion.doesHaveMane());
+    }
+
+    @Test
+    public void doesHaveManeFemaleReturnsFalse() throws Exception {
+        Lion lion = new Lion("Самка", feline);
+        Assert.assertFalse("Самка doesHaveMane() должен вернуть false", lion.doesHaveMane());
     }
 }
